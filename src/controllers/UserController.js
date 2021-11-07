23a1705a5a62a35b35f82a2ad09a2b40924471b8
Farruh Olimov <farruhbekolimov3904@gmail.com>
2021-11-07 20:15:56 +0500

    update validations

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,3 +1,16 @@
+const { UserSignUpValidation } = require("../modules/validations")
+
 module.exports = class UserController{
-    static async UserSignUpController(req, res, next){}
+    static async UserSignUpController(req, res, next){
+        const data = await UserSignUpValidation(req.body)
+
+        if(!data) throw new Error("Given data is not valid");
+
+    }
+    static async UserSignInController(req, res, next){
+        const data = await UserSignUpValidation(req.body)
+
+        if(!data) throw new Error("Given data is not valid");
+        
+    }
 }
--- src/modules/validations.js
+++ src/modules/validations.js
@@ -3,17 +3,17 @@ const joi = require("joi");
 module.exports = class Validations{
     static async UserSignUpValidation(data){
         return await joi.object({
-            username: joi.string().required().regex(/[A-Za-z]{2,}[_-]?[A-Za-z0-9]{2,}$/),
-            name: joi.string().required().min(5).max(64),
-            email: joi.string(),
-            password: joi.string().required().min(6).max(128),
-            gender: joi.string.required().valid("male", "female")
+            username: joi.string().required().regex(/[A-Za-z]{2,}[_-]?[A-Za-z0-9]{2,}$/).error(new Error("Username is not valid")),
+            name: joi.string().required().min(5).max(64).error(new Error("Name is not valid")),
+            email: joi.string().error(new Error("Email is not valid")),
+            password: joi.string().required().min(6).max(128).error(new Error("Password is not valid")),
+            gender: joi.string.required().valid("male", "female").error(new Error("This option is not available"))
         }).validateAsync(data)
     }
     static async UserSignInValidation(data){
         return await joi.object({
-            username: joi.string().required().regex(/[A-Za-z]{2,}[_-]?[A-Za-z0-9]{2,}$/),
-            password: joi.string().required().min(6).max(128),
+            username: joi.string().required().regex(/[A-Za-z]{2,}[_-]?[A-Za-z0-9]{2,}$/).error(new Error("Username is not valid")),
+            password: joi.string().required().min(6).max(128).error(new Error("Password is not valid")),
         }).validateAsync(data)
     }
 }
